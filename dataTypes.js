43968a29e25059a16c8591365d72c7bbce6fbd63
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-10-14 12:22:26 +0300

    add task 1 and 2

---
+++ dataTypes.js
@@ -0,0 +1,13 @@
+let integer = 31 ;
+let string = "Thes is a String ";
+let rray = ["first" ,2 ,3];
+const myName ={
+    firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+console.log(typeof integer);
+console.log(typeof string);
+console.log(typeof rray);
+console.log(typeof myName);
---
+++ myVariables.js
@@ -0,0 +1,15 @@
+let language ="JavaScript";
+let score =10;
+let length = 4;
+let width = 6 ;
+let area = length * width ;
+console.log(language);
+console.log(score);
+console.log(`The area of the rectangle is ${length} where ${width} = ${area}`);
+let num1 = 54 ;
+let num2 = 8 ;
+let y = num1 / num2 ;
+let x =  num1 % num2 ;
+console.log(`the dividing fo ${num1} by ${num2} = ${y}`);
+console.log( `the remainder is of dividing ${num1} by ${num2} = ${x} `);
+
